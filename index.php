6777dc4c6c242c718b1dd7b2bdae8581a71522fb
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-03 16:58:08 -0500

    added validation for the name and cupcake flavors, and displaying errors if information is not provided

--- index.php
+++ index.php
@@ -5,10 +5,6 @@
     A homepage for a cupcake shop allowing customers to order cupcakes
 -->
 
-<?php
-    $orders = array();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +12,64 @@
     <title>Cupcakes</title>
 </head>
 <body>
+
+    <?php
+        //If form is submitted, process the data
+
+        $isValid = true;
+
+        $flavors = "";
+        $orders = array();
+        $cupcakes = array("grasshopper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bacon",
+            "carrot"=>"Carrot Walnut", "caramel"=>"Salted Caramel Cupcake", "velvet"=>"Red Velvet",
+            "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
+
+        if (!empty($_POST))
+        {
+            //determine if the user entered valid data
+
+            //checking the name field
+            if (!empty($_POST['namex']))
+            {
+                $namex = $_POST['namex'];
+            }
+
+            else
+            {
+                print "<p>Please enter a name</p>";
+                $isValid = false;
+            }
+
+            //checking that at least one checkbox was checked
+
+            $validFlavors = array('grasshopper', 'maple', 'carrot', 'caramel', 'velvet', 'lemon', 'tiramisu');
+
+            if (isset($_POST['orders']))
+            {
+                for ($i = 0; $i <= count($_POST['orders']); $i++)
+                {
+                    if (in_array($_POST['orders'][$i], $validFlavors))
+                    {
+                        $flavors .= $_POST['orders'][$i] . " ";
+                    }
+
+                }
+            }
+
+            else
+            {
+                print "<p>Please choose at least one flavor</p>";
+                $isValid = false;
+            }
+        }
+
+        //if all the data is valid, display the requested information
+        if($isValid)
+        {
+
+        }
+    ?>
+
     <form id="cupcakesForm" method="post" action="#">
 
         <label>Name:&nbsp;<br>
@@ -26,10 +80,7 @@
         <br>
         <label>Cupcake Options</label><br>
         <?php
-            $cupcakes = array("grasshopper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bacon",
-                "carrot"=>"Carrot Walnut", "caramel"=>"Salted Caramel Cupcake", "velvet"=>"Red Velvet",
-                "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
-
+            //display cupcake flavors as checkboxes
             foreach ($cupcakes as $option => $text)
             {
                 echo "<label><input type='checkbox' ";
